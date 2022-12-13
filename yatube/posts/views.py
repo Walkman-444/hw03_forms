4a92197f4bbc27eefde932939c17d62d8ca06d67
Максим Иванов <ima_maxx@mail.ru>
2022-12-13 18:48:53 +0700

    прошел pytest. Отправка на ревью v 1.2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -62,7 +62,7 @@ def profile(request, username):
 
 
 def post_detail(request, post_id):
-    post=get_object_or_404(Post, pk=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     posts_count = post.author.posts.count()
     context = {
         'post': post,
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -13,67 +13,67 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-     # Страница регистрации - auth/signup/
+    # Страница регистрации - auth/signup/
     path('signup/', views.SignUp.as_view(), name='signup'),
-     # Шаблон, который отображает возвращаемую страницу.
+    # Шаблон, который отображает возвращаемую страницу.
     path(
-         'logout/',
-         LogoutView.as_view(template_name='users/logged_out.html'),
-         name='logout'
+        'logout/',
+        LogoutView.as_view(template_name='users/logged_out.html'),
+        name='logout'
     ),
     # Страница входа в учётную запись
     path(
-         'login/',
-         LoginView.as_view(template_name='users/login.html'),
-         name='login'
+        'login/',
+        LoginView.as_view(template_name='users/login.html'),
+        name='login'
     ),
     # Необязательные задания
     # Смена пароля: задать новый пароль
     path(
-         'password_change/',
-         PasswordChangeView.as_view(
-             template_name='users/password_change_form.html'),
-         name='password_change_form'
+        'password_change/',
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'),
+        name='password_change_form'
     ),
-        # Смена пароля: уведомление об удачной смене пароля
+    # Смена пароля: уведомление об удачной смене пароля
     path(
-         'password_change/done/',
-         PasswordChangeDoneView.as_view(
-             template_name='users/password_change_done.html'),
-         name='password_change_done'
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'),
+        name='password_change_done'
     ),
-      # Восстановление пароля:
-      # форма для восстановления пароля через email
+    # Восстановление пароля:
+    # форма для восстановления пароля через email
     path(
-         'password_reset/',
-         PasswordResetView.as_view(
-             template_name='users/password_reset_form.html'),
-         name='password_reset_form'
+        'password_reset/',
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'),
+        name='password_reset_form'
     ),
-      # Восстановление пароля:
-      # уведомление об отправке
-      # ссылки для восстановления пароля на email
+    # Восстановление пароля:
+    # уведомление об отправке
+    # ссылки для восстановления пароля на email
     path(
-         'password_reset/done/',
-         PasswordResetDoneView.as_view(
-             template_name='users/password_reset_done.html'),
-         name='password_reset_done'
+        'password_reset/done/',
+        PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'),
+        name='password_reset_done'
     ),
-      # Восстановление пароля:
-      # страница подтверждения сброса пароля;
-      # пользователь попадает сюда по ссылке из письма
+    # Восстановление пароля:
+    # страница подтверждения сброса пароля;
+    # пользователь попадает сюда по ссылке из письма
     path(
-         'reset/<uidb64>/<token>/',
-         PasswordResetConfirmView.as_view(
-             template_name='users/password_reset_confirm.html'),
-         name='password_reset_confirm.'
+        'reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'),
+        name='password_reset_confirm.'
     ),
-      # Восстановление пароля:
-      # уведомление о том, что пароль изменен
+    # Восстановление пароля:
+    # уведомление о том, что пароль изменен
     path(
-         'password_reset/done/',
-         PasswordResetCompleteView.as_view(
-             template_name='users/password_reset_complete.html'),
-         name='password_reset_complete'
+        'password_reset/done/',
+        PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'),
+        name='password_reset_complete'
     ),
 ]
